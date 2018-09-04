8b1cbd893341da3b37ec913ef0e5f2a7b53c3d80
Jaron Branham <jaronb91@yahoo.com>
2018-09-04 15:48:07 -0700

    Framed out CRUD for users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,30 @@
 class ArticlesController < ApplicationController
+
+  def new #page designed to aid in building a new entry
+
+  end
+
+  def create #action of building a new entry
+
+  end
+
+  def show #view the content of an entry
+
+  end
+
+  def index #view a collection of articles
+
+  end
+
+  def edit #page to aid in modifying an entry, similar to new
+
+  end
+
+  def update #action of modifying an entry
+
+  end
+
+  def destroy #removing entry from database
+
+  end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def new #page to create a new user
+
+  end
+
+  def create #action of building a new user
+
+  end
+
+  def show #profile edit page
+
+  end
+
+  def update #action of modifying a user
+
+  end
+
+  def destroy #removing user from database
+
+  end 
+
 end
