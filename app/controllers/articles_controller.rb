8d5a7856eafa83e14f3ebcd0a19ead5c95f96e49
Jaron Branham <jaronb91@yahoo.com>
2018-09-04 16:04:39 -0700

    CRUD routes added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,4 +27,8 @@ class ArticlesController < ApplicationController
   def destroy #removing entry from database
 
   end 
+
+  def landing #homepage/landing page
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/" => "articles#landing"
+  #users
+    get "/users" => "users#index"
+    get "/profile" => "users#show"
+    get "/profile/new" => "users#new"
+    post "/profile" => "users#create"
+    patch "/profile" => "users#update"
+    delete "/profile" => "users#destroy"
+  #articles
+    get "/articles" => "articles#index"
+    get "/articles/new" => "articles#new"
+    get "/articles/:id" => "articles#show"
+    get "/articles/:id/edit" => "articles#edit"
+    post "/articles" => "articles#create"
+    patch "/articles/:id" => "articles#update"
+    delete "/articles/:id" => "articles#destroy"
 end
