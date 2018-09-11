07215157c55ead884ae1e83ad23fa3fad30eb24e
Jaron Branham <jaronb91@yahoo.com>
2018-09-11 12:56:25 -0700

    created rails view pages and all controller renders & redirects. Tested base app functionality~ rendered landing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,34 +1,34 @@
 class ArticlesController < ApplicationController
 
   def new #page designed to aid in building a new entry
-
+    render :new
   end
 
   def create #action of building a new entry
-
+    redirect_to "/"
   end
 
   def show #view the content of an entry
-
+    render :show
   end
 
   def index #view a collection of articles
-
+    render :index
   end
 
   def edit #page to aid in modifying an entry, similar to new
-
+    render :edit
   end
 
   def update #action of modifying an entry
-
+    redirect_to "/"
   end
 
   def destroy #removing entry from database
-
+    redirect_to "/"
   end 
 
   def landing #homepage/landing page
-
+    render :landing
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
 
   def new #page to create a new user
-
+    render :new
   end
 
   def create #action of building a new user
-
+    redirect_to "/"
   end
 
   def show #profile edit page
-
+    render :show
   end
 
   def update #action of modifying a user
-
+    redirect_to "/"
   end
 
   def destroy #removing user from database
-
+    redirect_to "/"
   end 
 
 end
